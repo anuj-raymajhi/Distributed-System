a2c07a841ae7292b7682b949828d70321a4b839f
anuj_raymajhi <anujraymajhi@outlook.com>
2023-06-28 02:09:01 +0545

    cleaned some comments

--- server/dbProcess.java
+++ server/dbProcess.java
@@ -5,19 +5,11 @@ import java.rmi.RemoteException;
 public interface dbProcess extends Remote {
     // Defining behaviors/methods
     // writing methods
-    // public void addName(String name) throws RemoteException;
-    // public void addAddress(String address) throws RemoteException;
-    // public void addRollNo(String roll) throws RemoteException;
-    // public void addGrade(int grade) throws RemoteException;
 
     public void addRecord(String name, String address, String roll, int grade) throws RemoteException;
 
     // reading methods
-    // public String getName() throws RemoteException;
-    // public String getAddress() throws RemoteException;
-    // public String getRollNo() throws RemoteException;
-    // public int getGrade() throws RemoteException;
-
+    
     public String getRecord(String roll) throws RemoteException;
 
 }
